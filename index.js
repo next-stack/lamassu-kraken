405ce8830ae58bbaf09c0c0186fb672bdd1c17e8
Josh Harvey <josh@lamassu.is>
2016-04-04 21:43:11 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var _ = require('lodash')
+
+_.merge(exports, require('./config'))
+
+// Ticker merhods:
+exports.ticker = require('./lib/ticker').ticker
+
+// Trader methods:
+var trader = require('./lib/trader')
+exports.purchase = trader.purchase
+exports.sell = trader.sell
