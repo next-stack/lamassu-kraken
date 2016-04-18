e24164ab94fad5a3c651a0e73104c1cd72f47b71
Josh Harvey <josh@lamassu.is>
2016-04-18 20:34:04 +0300

    remove debug

--- config.js
+++ config.js
@@ -6,6 +6,5 @@ exports.NAME = 'Kraken'
 exports.SUPPORTED_MODULES = ['ticker', 'trader']
 
 exports.config = function config (localConfig) {
-  console.log(localConfig)
   if (localConfig) trader.config(localConfig)
 }
